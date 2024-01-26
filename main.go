f09edaf67f7af7064acaf63a73d8f8c6c971f205
max-wacquier <max.wacquier@gmail.com>
2024-01-26 15:25:59 +0100

    cors config added

--- main.go
+++ main.go
@@ -2,12 +2,36 @@ package main
 
 import (
     "github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
+}
 
 // main is the entry point for the application.
 func main() {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+	// router.Use(cors.Default())
 	router.GET("/users", readUser)
 	router.POST("/users", createUser)
 	router.PUT("/users/:id", updateUser)
